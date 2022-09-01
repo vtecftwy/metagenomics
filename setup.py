55cc947688673f91e6ada499fd9cac196557b895
vtecftwy <github@procurasia.com>
2022-09-01 10:59:55 +0800

    Correct setup.py file

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 setup(name='src',
       version='1.0.0',
       description='Specific code in development for this project',
-      py_modules=['src'],
+      packages=['src'],
       )
