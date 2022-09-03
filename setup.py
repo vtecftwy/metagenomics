e2fe42370994b8c67d1e2acfc185fa809851d852
vtecftwy <github@procurasia.com>
2022-09-03 13:01:52 +0800

    increase version in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='src',
-      version='1.0.0',
+      version='1.0.1',
       description='Specific code in development for this project',
       packages=['src'],
       )
