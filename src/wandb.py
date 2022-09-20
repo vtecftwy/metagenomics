e250a4c80f67e106e4a8550040fe16580c61c5da
vtecftwy <github@procurasia.com>
2022-09-20 14:03:25 +0800

    Action plan for wandb.py

---
+++ src/wandb.py
@@ -0,0 +1,17 @@
+# Classes and functions to support WandB process
+# COMMENT: What to do in this module?
+# [ ]   Define main operations involving WandB
+#       - preparing dataset and loading datasets as artifact
+#       - training a model with a dataset and hyperparameters
+#       - testing / validating model with test dataset
+#       - infering with now data
+# 
+# 
+# [ ]   Function required in dataset preparation -> raw_data, processed_data, code, 
+# 
+# [ ]   Function required in model training -> dataset, code, model, hyperparameters
+#       - how to track epochs, learning rates and other 
+#       - how to track gradients and activations
+# 
+# [ ]   Function required in model testing -> dataset, code, trained model, preformance and reports
+
