a242e7b144ca07f1e47ea0f77e4c59905cc2bc36
vtecftwy <github@procurasia.com>
2022-10-03 17:57:06 +0800

    increment package version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='src',
-      version='1.0.4',
+      version='1.0.5',
       description='Specific code in development for this project',
       packages=['src'],
       )
